a7bbb4453aeede8f0462438d84b645b6c051ae33
letsbreelhere <letsbreelhere@gmail.com>
2022-02-14 11:55:23 -0800

    Final jeopardy sounds

--- src/renderer/FinalJeopardy.tsx
+++ src/renderer/FinalJeopardy.tsx
@@ -2,17 +2,36 @@ import { useCallback, useEffect, useContext, useState } from 'react';
 
 import { ReducerContext } from './reducer';
 import useKeyEvent from './useKeyEvent';
+import audio from './audio';
+
+const nextStep: { [name: string]: string } = {
+  CATEGORY: 'CLUE',
+  CLUE: 'ANSWERING',
+};
 
 const FinalJeopardy = () => {
   const [step, setStep] = useState('CATEGORY');
   const { state } = useContext(ReducerContext);
 
-  const onKeyPressed = useCallback((key) => {
-    if (key === 'space') {
-      setStep('CLUE');
-    }
-  }, []);
+  const onKeyPressed = useCallback(
+    (key) => {
+      if (key === 'space' && nextStep[step]) {
+        setStep(nextStep[step]);
+      }
+    },
+    [step]
+  );
   useKeyEvent(onKeyPressed);
+
+  useEffect(() => {
+    if (step === 'CLUE') {
+      audio.play('reveal');
+    } else if (step === 'ANSWERING') {
+      audio.play('final_jeopardy');
+    }
+
+    return audio.stop;
+  }, [step]);
 
   return (
     <>
