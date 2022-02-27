992defe0c411b8b7eb62e28ccd37b37b4e21b4f3
letsbreelhere <letsbreelhere@gmail.com>
2022-02-27 12:11:49 -0800

    Horrible fix for sound bug
    In short, attempting to play a sound on app load causes problems for
    reasons that are mysterious to me. Tying the intro music to a mouse
    click solves the problem, but more ideally I could hack around this in
    an automatic way so as not to require user input.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,25 +1,28 @@
 import { useState, useEffect, useReducer } from 'react';
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  useNavigate,
+  MemoryRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import Mousetrap from 'mousetrap';
 
 import { EventRegister } from 'react-native-event-listeners';
 import Settings from './Settings';
 import Setup from './Setup';
 import MainGame from './MainGame';
-import { reducer, initialState, derivedState } from './reducer';
-import { ReducerContext } from './reducer';
-import audio from './audio';
+import { ReducerContext, reducer, initialState, derivedState } from './reducer';
 
-const keysToWatch = [
-  'f1',
-  'f2',
-  'f3',
-  'f4',
-  'space',
-  'escape',
-  'y',
-  'n',
-];
+const keysToWatch = ['f1', 'f2', 'f3', 'f4', 'space', 'escape', 'y', 'n'];
+
+const Init = () => {
+  const navigate = useNavigate();
+  const onClick = () => navigate('/setup', { replace: true });
+
+  return (
+    <div style={{ width: '100vw', height: '100vh' }} onClick={onClick}></div>
+  );
+};
 
 const App = () => {
   useEffect(() => {
@@ -30,12 +33,12 @@ const App = () => {
     });
   }, []);
 
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [showPrefs, setShowPrefs] = useState(false);
+
   useEffect(() => {
     electron.ipc.on('openSettings', () => setShowPrefs(true));
   }, []);
-
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const [showPrefs, setShowPrefs] = useState(false);
 
   return (
     <>
@@ -43,7 +46,8 @@ const App = () => {
       <ReducerContext.Provider value={{ state: derivedState(state), dispatch }}>
         <Router>
           <Routes>
-            <Route path="/" element={<Setup />} />
+            <Route path="/" element={<Init />} />
+            <Route path="/setup" element={<Setup />} />
             <Route path="/game" element={<MainGame />} />
           </Routes>
         </Router>
